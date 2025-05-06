4fbf84092c0ea3556c54a20020f2cadf4b75614c
Vivek Yanamadula <vivekyanamadula@gmail.com>
2025-05-06 03:49:46 -0400

    Add last updated tag to bottom right of page

--- src/app/_components/pageContent.tsx
+++ src/app/_components/pageContent.tsx
@@ -4,6 +4,11 @@ export function PageContent({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col items-center justify-center w-full h-[calc(100svh-3.5rem)]">
       {children}
+      <div className="absolute bottom-0 right-2">
+        <span className="text-gray-500 dark:text-gray-400 text-sm italic">
+          Last updated: 5/6/2025
+        </span>
+      </div>
     </div>
   );
 }
