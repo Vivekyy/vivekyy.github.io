fb82ab11e955c1e9b8f89367e34f1e571b7d4464
Vivek Yanamadula <vivekyanamadula@gmail.com>
2025-05-14 04:04:10 -0400

    Fix pageContent height calculation

--- src/app/_components/pageContent.tsx
+++ src/app/_components/pageContent.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export async function PageContent({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen">
+    <div className="flex flex-col items-center justify-center w-full h-[calc(100vh-3.5rem)]">
       {children}
     </div>
   );
