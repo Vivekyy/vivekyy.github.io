245dd2fc47c0e470370107e59a4ca88c1e5891d5
Vivek Yanamadula <vivekyanamadula@gmail.com>
2025-05-23 00:15:46 -0400

    Add shadow to images for display in light mode

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -57,7 +57,7 @@ function ProjectCard({title, description, image, link}: {title: string, descript
   return (
     <div className='w-full'>
       <Card padding="p-10">
-        <div className={`relative h-48 w-full mb-4 ${link && 'hover:h-60 transition-all duration-300 ease-in-out'}`}>
+        <div className={`relative h-48 w-full mb-4 shadow-md ${link && 'hover:h-60 transition-all duration-300 ease-in-out'}`}>
           <a href={link} target='_blank' rel="noopener noreferrer">
             <Image src={image} alt={title} className="w-full h-48 object-cover rounded-lg mb-4" fill={true}/>
           </a>
