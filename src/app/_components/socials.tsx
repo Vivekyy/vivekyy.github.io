b74bb8b9bf0d77ad3644e8d699d72772192e746f
Vivek Yanamadula <vivekyanamadula@gmail.com>
2025-06-12 00:15:45 -0400

    Reorder socials

--- src/app/_components/socials.tsx
+++ src/app/_components/socials.tsx
@@ -8,10 +8,10 @@ export function Socials({iconClassName}: {iconClassName: string}) {
   const { Email, LinkedIn, Github, Instagram } = socials.data;
   return (
     <div className='inline-flex text-lg text-center text-gray-600 dark:text-gray-400'>
-      <ExternalLink href={Email} className={iconClassName}><FontAwesomeIcon icon={faEnvelope} /></ExternalLink>
-      <ExternalLink href={LinkedIn} className={iconClassName}><FontAwesomeIcon icon={faLinkedin} /></ExternalLink>
       <ExternalLink href={Github} className={iconClassName}><FontAwesomeIcon icon={faGithub} /></ExternalLink>
       <ExternalLink href={Instagram} className={iconClassName}><FontAwesomeIcon icon={faInstagram} /></ExternalLink>
+      <ExternalLink href={LinkedIn} className={iconClassName}><FontAwesomeIcon icon={faLinkedin} /></ExternalLink>
+      <ExternalLink href={Email} className={iconClassName}><FontAwesomeIcon icon={faEnvelope} /></ExternalLink>
     </div>
   );
 }
