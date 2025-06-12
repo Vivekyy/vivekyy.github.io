bc949d5b457dc294bc2f471d77e480a2d7251504
Vivek Yanamadula <vivekyanamadula@gmail.com>
2025-06-12 00:28:11 -0400

    Change favicon and site tab title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: 'Vivek Yanamadula',
+  title: 'Vivek\'s Website',
   description: 'Personal website of Vivek Yanamadula',
 };
 
